768ff9ac7779d99b0ec4f91bb27f33a906f49ad3
Alexander Vitchinov <vitalex92@yandex.ru>
2021-06-27 07:49:27 +0300

    preloader spinner

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { Button, Alert, Breadcrumb } from 'react-bootstrap';
+import Preloader from '../Preloader/Preloader';
 
 function App() {
   return (
     <div className="App">
       <div className='page'>
+        <Alert variant='success'>Hour</Alert>  
+      <Button>Test</Button>
+      <Breadcrumb>
+        <Breadcrumb.Item active>Test</Breadcrumb.Item>
+        <Breadcrumb.Item>Test 1</Breadcrumb.Item>
+        <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+      </Breadcrumb>
       </div>
+      <Preloader />
     </div>
   );
 }
---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,19 @@
+function Preloader() {
+  return (
+    <section class='loader loader__spinner'>
+  <div>
+    <div>
+      <div>
+        <div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  ) 
+}
+
+export default Preloader;
